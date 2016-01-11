13245422ed872d901b0730470c61094bf1dd7f4b
eparkhurst <eli.parkhurst@gmail.com>
2016-01-11 14:10:41 -0700

    reset knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,13 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgress://localhost/gitUp'
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE
+    },
+    migrations: {tableNanme: 'knex_migrations'}
   },
 
   production: {
