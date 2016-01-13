ca2febae406f37c76f31deb358a01f4a11908f88
eparkhurst <eli.parkhurst@gmail.com>
2016-01-13 14:13:34 -0700

    fixed knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,14 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection:  {
+    connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_DATABASE
     },
-    migrations: {tableNanme: 'knex_migrations'},
+    migrations: {tableNanme: 'knex_migrations'}
+  },
 
   production: {
     client: 'pg',
